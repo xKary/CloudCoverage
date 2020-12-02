ba05ffe053784db82fe430afffc5df22ed77b28f
xKary <karyxprado@cienias.unam.mx>
2020-12-01 22:46:09 -0600

    Comen

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -10,38 +10,42 @@ import java.util.LinkedList;
 import k_means.*;
 
 public class App {
+
     public static String folder = "src/main/resources/";
+    public static String inputName = folder;
+    public static String outputName = folder;
+    public static boolean generateImg = false;
+
     public static void main(String args[])throws IOException {
 
         BufferedImage img = null;
-        File f = null;
-        boolean generateImg = false;
-        String inputName = folder + args[0];
-        String outputName = "nitida_clustered.jpg";
 
-        if(args.length > 1){
-            if(args[1].toLowerCase() == "s")
-            generateImg = true;
+        try {
+            checkParameters(args);
+        } catch (Exception e) {
+            System.out.println("Ingresa los parámetros solicitados");
+            return;
         }
 
         try {
-            f = new File(inputName);
+            File f = new File(inputName);
             img = ImageIO.read(f);
-            System.out.println("Lei la imagen");
         }
         catch(IOException e) {
+            System.out.println("Ha ocurrido un error con el archivo.");
             System.out.println(e);
         }
 
         int img_width = img.getWidth();
         int img_height = img.getHeight();
 
+        //cambiar el nombre de puntos
         LinkedList<RGBDot> puntos = new LinkedList<RGBDot>();
         for(int i = 0; i < img_width; i++) {
             for(int j = 0; j < img_height; j++) {
                 double d = Math.sqrt(Math.pow(2184 - i,2) + Math.pow(1456 - j,2));
                 if(d <= 1324)
-                puntos.add(new RGBDot(i,j, img.getRGB(i, j)));
+                    puntos.add(new RGBDot(i,j, img.getRGB(i, j)));
             }
         }
 
@@ -76,6 +80,28 @@ public class App {
         }
     }
 
+    /**
+     * Método que verifica que se reciban los parámetros solitcitados.
+     * @param args Arreglo que contiene los parámetros solicitados.
+     */
+    public static void checkParameters(String[] args){
+        inputName += args[0];
+        outputName += inputName + "-seg.jpg";
+
+        char flag = args[1].charAt(0);
+
+        if(args.length > 1)
+            if(flag == 's' || flag == 'S')
+                generateImg = true;
+        System.out.println(generateImg);
+
+    }
+
+    /**
+     * Método que calcula el Índice de Cobertura Nubosa de la imagen.
+     * @param pixeles Lista de pixeles que conforman la imagen.
+     * @return float  Índice de cobertura nubosa.
+     */
     public static float calculateIcc(LinkedList<LinkedList<RGBDot>> pixeles) {
         int cloud_n = pixeles.get(0).size();
         int sky_n = pixeles.get(1).size();
@@ -83,6 +109,13 @@ public class App {
         return icc;
     }
 
+    /**
+     * Método que genera la ima
+     * @param clustered
+     * @param width Ancho de la imagen.
+     * @param height Alto de la imagen.
+     * @return BufferedImage
+     */
     public static BufferedImage generateClusteredImg(LinkedList<LinkedList<RGBDot>> clustered, int width, int height) {
         BufferedImage clustered_img = new BufferedImage(width, height,  BufferedImage.TYPE_INT_RGB);
 
